b669e2c03f005fa7da2b7527accff54a036675d7
gomding <backchida@gmail.com>
2022-01-17 02:53:22 +0900

    refactor: AccountIdTest 테스트명 수정

--- src/main/java/com/momsitter/domain/AccountId.java
+++ src/main/java/com/momsitter/domain/AccountId.java
@@ -4,10 +4,10 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class AccountId {
-    public static final String NULL_EXCEPTION_MESSAGE = "사용자의 계정 ID는 null일 수 없습니다.";
-    public static final String LENGTH_EXCEPTION_MESSAGE_FORMAT = "사용자의 계정 ID는 6글자 이상 20글자 이하여야 합니다. (현재 길이 : %d)";
-    public static final String BLANK_EXCEPTION_MESSAGE = "사용자의 계정 ID는 공백을 포함할 수 없습니다.";
-    public static final String PATTERN_EXCEPTION_MESSAGE = "사용자의 계정 ID는 영어 또는 숫자만 입력 가능합니다.";
+    public static final String NULL_EXCEPTION_MESSAGE = "아이디는 null일 수 없습니다.";
+    public static final String LENGTH_EXCEPTION_MESSAGE_FORMAT = "아이디는 6글자 이상 20글자 이하여야 합니다.(현재 길이 : %d)";
+    public static final String BLANK_EXCEPTION_MESSAGE = "아이디는 공백을 포함할 수 없습니다.";
+    public static final String PATTERN_EXCEPTION_MESSAGE = "아이디는 영어 또는 숫자만 입력 가능합니다.";
     public static final Pattern PATTERN = Pattern.compile("^[a-zA-Z0-9]*$");
     private static final int MIN_LENGTH = 6;
     private static final int MAX_LENGTH = 20;
@@ -18,14 +18,8 @@ public class AccountId {
     public AccountId(String value) {
         this.value = Objects.requireNonNull(value, NULL_EXCEPTION_MESSAGE);
         validateLength(this.value.length());
-        validateContainBlank(this.value);
+        validateContainsBlank(this.value);
         validatePattern(this.value);
-    }
-
-    private void validatePattern(String value) {
-        if (!PATTERN.matcher(value).matches()) {
-            throw new IllegalArgumentException(PATTERN_EXCEPTION_MESSAGE);
-        }
     }
 
     private void validateLength(int length) {
@@ -34,9 +28,15 @@ public class AccountId {
         }
     }
 
-    private void validateContainBlank(String value) {
+    private void validateContainsBlank(String value) {
         if (value.contains(BLANK)) {
             throw new IllegalArgumentException(BLANK_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validatePattern(String value) {
+        if (!PATTERN.matcher(value).matches()) {
+            throw new IllegalArgumentException(PATTERN_EXCEPTION_MESSAGE);
         }
     }
 
--- src/test/java/com/momsitter/domain/AccountIdTest.java
+++ src/test/java/com/momsitter/domain/AccountIdTest.java
@@ -17,7 +17,7 @@ public class AccountIdTest {
     @Nested
     class CreateTest {
 
-        @DisplayName("계정 ID 정상 생성된다.")
+        @DisplayName("회원 계정 ID 정상 생성된다.")
         @Test
         void create() {
             // given
@@ -27,38 +27,35 @@ public class AccountIdTest {
             AccountId accountId = new AccountId(value);
 
             // then
-            assertThat(accountId.getValue()).isEqualTo(value);
+            assertThat(accountId).isEqualTo(new AccountId(value));
         }
 
-        @DisplayName("사용자의 계정 ID가 null이면 예외가 발생한다.")
+        @DisplayName("회원의 계정 ID가 null이면 예외가 발생한다.")
         @Test
         void createAccountIdIsNull() {
-            // given
-            String value = null;
-
-            // when
-            assertThatThrownBy(() -> new AccountId(value))
+            // when then
+            assertThatThrownBy(() -> new AccountId(null))
                     .isInstanceOf(NullPointerException.class)
                     .hasMessage(NULL_EXCEPTION_MESSAGE);
         }
 
-        @DisplayName("사용자의 계정 ID가 6글자 미만이면 예외가 발생한다.")
+        @DisplayName("회원의 계정 ID가 6글자 미만이면 예외가 발생한다.")
         @Test
-        void createAccountIdMinLength() {
+        void createMinLength() {
             // given
             String value = "fiveL";
 
-            // when
+            // when then
             assertThatThrownBy(() -> new AccountId(value))
                     .isExactlyInstanceOf(IllegalArgumentException.class)
                     .hasMessage(String.format(LENGTH_EXCEPTION_MESSAGE_FORMAT, value.length()));
         }
 
-        @DisplayName("사용자의 계정 ID가 20글자를 초과하면 예외가 발생한다.")
+        @DisplayName("회원의 계정 ID가 20글자를 초과하면 예외가 발생한다.")
         @Test
-        void createAccountIdMaxLength() {
+        void createMaxLength() {
             // given
-            String value = "thisAccountIdIsTooMuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuchLong";
+            String value = "TooMuuuuuuuuuuuchLong";
 
             // when
             assertThatThrownBy(() -> new AccountId(value))
@@ -66,9 +63,9 @@ public class AccountIdTest {
                     .hasMessage(String.format(LENGTH_EXCEPTION_MESSAGE_FORMAT, value.length()));
         }
 
-        @DisplayName("사용자의 계정 ID에 공백(\" \")이 포함되면 예외가 발생한다.")
+        @DisplayName("회원의 계정 ID에 공백(\" \")이 포함되면 예외가 발생한다.")
         @Test
-        void createAccountIdContainBlank() {
+        void createContainsBlank() {
             // given
             String value = "have  Blank";
 
@@ -78,10 +75,10 @@ public class AccountIdTest {
                     .hasMessage(BLANK_EXCEPTION_MESSAGE);
         }
 
-        @DisplayName("계정 ID에 영어 또는 숫자외의 값이 들어가면 예외가 발생한다.")
+        @DisplayName("회원의 계정 ID에 영어 또는 숫자외의 값이 들어가면 예외가 발생한다.")
         @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
         @CsvSource({"mom2022한글이들어간아이디", "momsitter2022!!!#@"})
-        void createMissPattern(String value) {
+        void createMissMatchPattern(String value) {
             // when then
             assertThatThrownBy(() -> new AccountId(value))
                     .isExactlyInstanceOf(IllegalArgumentException.class)
