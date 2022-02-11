6c4db66a2ccb3b817b2687c8bc7d71439b821a06
Gomding <backchida@gmail.com>
2022-02-11 14:08:41 +0900

    refactor : ControllerAdvice -> ExceptionAdvice 로 이름 변경
    예외를 주로 핸들링 하므로 ControllerAdvice 보다는 ExceptionAdvice가 나을거라 생각해서 변경

---
+++ assignment-api/src/main/java/com/momsitter/api/ui/ExceptionAdvice.java
@@ -0,0 +1,35 @@
+package com.momsitter.api.ui;
+
+import com.momsitter.common.exception.ExceptionResponse;
+import com.momsitter.common.exception.MomSitterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+    private final Logger log = LoggerFactory.getLogger(ExceptionAdvice.class);
+
+    @ExceptionHandler(MomSitterException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MomSitterException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(e.getHttpStatus()).body(e.getBody());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(Exception e) {
+        log.error(e.getMessage());
+        StringBuilder sb = new StringBuilder();
+        String[] spiltMessages = Arrays.toString(e.getStackTrace()).split(",");
+        for (String splitMessage : spiltMessages) {
+            sb.append(splitMessage).append("\n");
+        }
+        log.error(sb.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("맘시터 관리자에게 문의해주세요");
+    }
+}
