d614ff7b12f704328defc083d3e248d1ae116a15
Gomding <backchida@gmail.com>
2022-02-14 18:16:32 +0900

    refactor(account-api) : AuthorizationExtractor 내부의 사용하지 않는 코드 제거
    아래 코드는 사용되지 않는 코드로 삭제처리
    request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length()).trim());

--- account-api/src/main/java/com/momsitter/api/infrastructure/AuthorizationExtractor.java
+++ account-api/src/main/java/com/momsitter/api/infrastructure/AuthorizationExtractor.java
@@ -6,7 +6,6 @@ import java.util.Enumeration;
 public class AuthorizationExtractor {
 
     private static final String AUTHORIZATION = "Authorization";
-    private static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
     private static final String BEARER_TYPE = "Bearer";
 
     private AuthorizationExtractor() {
@@ -17,7 +16,7 @@ public class AuthorizationExtractor {
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
             if (isBearerHeader(value)) {
-                return authHeaderValue(request, value);
+                return extractTokenFromHeaderValue(value);
             }
         }
 
@@ -28,9 +27,8 @@ public class AuthorizationExtractor {
         return value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase());
     }
 
-    private static String authHeaderValue(HttpServletRequest request, String value) {
+    private static String extractTokenFromHeaderValue(String value) {
         String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
-        request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length()).trim());
         int commaIndex = authHeaderValue.indexOf(',');
         if (commaIndex > 0) {
             authHeaderValue = authHeaderValue.substring(0, commaIndex);
