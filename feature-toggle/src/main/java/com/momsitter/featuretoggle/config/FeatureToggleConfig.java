5a2b64660ff61c3d266ac2faa224afc2f29da452
Gomding <backchida@gmail.com>
2022-02-25 14:38:43 +0900

    feat(featrue-toggle): feature toggle 설정에 logger 추가

--- feature-toggle/src/main/java/com/momsitter/featuretoggle/config/FeatureToggleConfig.java
+++ feature-toggle/src/main/java/com/momsitter/featuretoggle/config/FeatureToggleConfig.java
@@ -4,6 +4,8 @@ import com.momsitter.featuretoggle.config.property.UnleashProperties;
 import io.getunleash.DefaultUnleash;
 import io.getunleash.Unleash;
 import io.getunleash.util.UnleashConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,10 +16,13 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(UnleashProperties.class)
 public class FeatureToggleConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureToggleConfig.class);
+
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnProperty(prefix = "momsitter.feature.toggle", name = "vendor", havingValue = "unleash")
     public Unleash unleashFeatureToggle(UnleashProperties properties) {
+        LOGGER.info("initializing Unleash Feature Toggle Config");
         UnleashConfig unleashConfig = UnleashConfig.builder()
                 .appName(properties.getAppName())
                 .instanceId(properties.getInstanceId())
